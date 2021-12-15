0b8baf67ae4090e5de96519450d52fee125d796c
Udennaka Marktony <chukwudiudennaka@gmail.com>
2021-12-15 13:35:33 +0100

    if applied this commit shows a list of 10 (ten) pokemon characters with different property variables.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,13 @@
-alert('Hello world');
+// a list of pokemons with different property variables
+let pokemonList = [{name:'charizard', height: 1.7, weight: 90.5, type: ['fire', 'flying']}, 
+    {name:'butterfree', height: 1.1, weight: 32,  type: ['bug', 'flying']},
+    {name:'weedle', height: 0.3, weight: 3.2,  type: ['bug', 'poison']},
+    {name:'pidgey', height: 0.3, weight: 1.8,  type: ['flying', 'normal']},
+    {name:'spearow', height: 0.3, weight: 2,  type: ['flying', 'normal']},
+    {name:'nidoqueen', height: 1.3, weight: 60,  type: ['ground', 'poison']},
+    {name:'nidoking', height: 1.4, weight: 62,  type: ['ground', 'poison']},
+    {name:'wigglytuff', height: 1, weight: 12,  type: ['fairy', 'normal']},
+    {name:'golbat', height: 1.6, weight: 55,  type: ['poison', 'flying']},
+    {name:'oddish', height: 0.5, weight: 5.4,  type: ['grass', 'poison']},
 
-// conventional camelCase naming 
-let favoriteFood = 'Fried rice';
-document.write(favoriteFood) ;
+]
