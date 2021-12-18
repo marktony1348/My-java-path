8e1484b6692320ba1374e54d413bd7dbfd401cbd
Udennaka Marktony <chukwudiudennaka@gmail.com>
2021-12-18 15:36:21 +0100

    this commit adds design changes (logo, background image and background colour) to the css and html file while changes where effected in the js-file in terms of addition of html tags in the js strings

--- js/script.js
+++ js/script.js
@@ -8,10 +8,27 @@ let pokemonList = [
 
 // creating for loop (variable initialization: let i=0), (conditional: i < pokemonList.lenght ),(action: i = i+1 or i++)
 for (let i = 0; i < pokemonList.length; i++) {
-    document.write('<br>' + pokemonList[i].name + (' , height: ') + pokemonList[i].height);
+    // html tags that are placed in the javascript strings as represented 
+    // in all the conditional statements below
+
+    document.write('<p style= "color: blue;"> <br>' + pokemonList[i].name + ('  (height: ') + pokemonList[i].height + ')</p>');
     
     // for display on the console log
     console.log(pokemonList[i].name + (' , height: ') + pokemonList[i].height);
+
+    // creating the if-else conditional statements for the height classification
+    if (pokemonList[i].height >= 2) {
+
+        
+        document.write('<i> - Wow, that\'s big</i>');
+    } 
+    // this area shows the median height of the 3 pokemons in the list
+    else if (pokemonList[i].height > 1 || pokemonList[i] < 2 ) {
+        document.write('<i> This is a medium-sized pokemon</i>');
+    }
+    else {
+        document.write('<i> This is a small pokemon </i>');
+    }
     
 }
-   
+  
