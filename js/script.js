9daa3ba5cf4ae44d5703bcd559001e2b9564fddb
Udennaka Marktony <chukwudiudennaka@gmail.com>
2021-12-23 19:21:29 +0100

    this commit adds the scond phase IIFE to the array list i the js file

--- js/script.js
+++ js/script.js
@@ -6,6 +6,32 @@ let pokemonList = [
     {name:'venusaur', height: 2,   type: ['grass', 'poison']}
 ];
 
+// creating for loop (variable initialization: let i=0), (conditional: i < pokemonList.lenght ),(action: i = i+1 or i++)
+for (let i = 0; i < pokemonList.length; i++) {
+    // html tags that are placed in the javascript strings as represented 
+    // in all the conditional statements below
+
+    document.write('<p style= "color: blue;"> <br>' + pokemonList[i].name + ('  (height: ') + pokemonList[i].height + ')</p>');
+    
+    // for display on the console log
+    console.log(pokemonList[i].name + (' , height: ') + pokemonList[i].height);
+
+    // creating the if-else conditional statements for the height classification
+    if (pokemonList[i].height >= 2) {
+
+        
+        document.write('<i> - Wow, that\'s big</i>');
+    } 
+    // this area shows the median height of the 3 pokemons in the list
+    else if (pokemonList[i].height > 1 || pokemonList[i] < 2 ) {
+        document.write('<i> This is a medium-sized pokemon</i>');
+    }
+    else {
+        document.write('<i> This is a small pokemon </i>');
+    }
+    
+}
+  
 
 // for each loop
 pokemonList.forEach(function(pokemon) {
@@ -14,3 +40,30 @@ pokemonList.forEach(function(pokemon) {
  });
      
   
+// 2nd pahse IIFE (functional programming)
+let pokemonRepository = (function () {
+    let pokemonList = [
+        {name:'charizard', height: 1.7,  type: ['fire', 'flying']}, 
+        {name:'wingull', height: 0.6,   type: ['fairy', 'normal']}, 
+        {name:'venusaur', height: 2,   type: ['grass', 'poison']}
+        
+        ];
+
+// public function creation (getAll and add)
+    return {
+      add: function(pokemon) {
+        pokemonList.push(pokemon);
+      },
+      getAll: function() {
+        return pokemonList;
+      }
+    };
+       
+})();
+
+// addition of a new pokemon to the array list
+console.log(pokemonRepository.add([{name:'Rhyperior', height: 2.4,   type: ['rock', 'ground']}]));
+
+// printing the total array list with the updated pokemon object
+console.log(pokemonRepository.getAll());
+
